8cfdb03dfaeb3e802dde4093eac2edb5c874e954
vadyk <vadykdeejay@gmail.com>
2026-02-03 18:05:05 +0200

    feat: integrate all components into main app layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
