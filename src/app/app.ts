bfd9bf8567dd799ae129041e1612463204708e28
vadyk <vadykdeejay@gmail.com>
2026-02-04 18:07:23 +0200

    chore: update app component and cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,14 @@ import { Navbar } from './components/navbar/navbar';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
 import { Skills } from './components/skills/skills';
+import { Portfolio } from './components/portfolio/portfolio';
+import { Testimonials } from './components/testimonials/testimonials';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, About, Skills],
+  imports: [RouterOutlet, Navbar, Hero, About, Skills, Portfolio, Testimonials, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
