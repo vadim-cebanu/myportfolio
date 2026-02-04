fc81ff3f9c1d4b9dd2d0593516f06ce267e0cee1
vadyk <vadykdeejay@gmail.com>
2026-02-04 17:16:39 +0200

    feat: add legal notice and privacy pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'legal', component: LegalNotice },
+  { path: 'privacy', component: PrivacyPolicy }
+];
